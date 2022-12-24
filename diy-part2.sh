56a42e7ce3dc4c1fc6f1216bfd750f48d77028d4
chika0801 <88967758+chika0801@users.noreply.github.com>
2022-12-25 00:26:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,2 @@
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/root:::0:99999:7:::/root:$1$M8troG2V$m2hoYp9yvgk91n1jU7Ssb1:19157:0:99999:7:::/g' package/base-files/files/etc/shadow
