bd204c0501889312b8109f8ec44aeeef84fa551e
chika0801 <88967758+chika0801@users.noreply.github.com>
2022-12-26 11:54:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,2 +1,2 @@
-sed -i 's/192.168.6.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 sed -i 's/root::0:0:99999:7:::/root:$1$M8troG2V$m2hoYp9yvgk91n1jU7Ssb1:19157:0:99999:7:::/g' package/base-files/files/etc/shadow
